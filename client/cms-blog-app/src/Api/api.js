fe0b497c88cb9147c80978c5e1b47ca0b9179a61
Huzefah-javed <huzefahjaved@gmail.com>
2025-08-27 12:04:07 +0500

    API link setting up

--- client/cms-blog-app/src/Api/api.js
+++ client/cms-blog-app/src/Api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "cms-blog-app-mern.railway.internal"
+    baseURL: "blog-mern-api.com/"
 })
 
 export const adminDashboardData =async()=>{
